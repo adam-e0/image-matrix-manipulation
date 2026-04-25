ec555c999b364fb8b8b2a5f9062129079ae8ae69
Adam E <77598041+adam-e0@users.noreply.github.com>
2026-04-24 21:40:09 -0600

    fft stuff

--- functiosn.py
+++ functiosn.py
@@ -1,15 +1,24 @@
+import numpy as np
 from PIL import Image
 
 
-def processImage(inputPath):
+def getImage(inputPath):
+    img = Image.open(inputPath)
+    return img
+
+
+def processImage(inputPath, freqCutOff):
     img = Image.open(inputPath)
     print(f"Successfully loaded: {inputPath}")
     print(f"Format: {img.format}")
     print(f"Size: {img.size}")
     print(f"Mode: {img.mode}")
-    new_size = (400, 100)
-    resized_img = img.resize(new_size)
-    resized_img.show()
+    imgArr = np.array(img)
+    imgFft = np.fft.fft2(imgArr)
+
+    # Add here
+
+    print(imgFft)
 
 
-processImage("./images/flower.jpg")
+processImage("./images/flower.jpg", 0.5)
